516afe35adf8b4a6bdd528171b0041261e543088
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2025-12-23 15:51:19 +0530

    one another componenets create

---
+++ app/components/SearchBar.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+export default function SearchBar({ onSearch }) {
+  const [username, setUsername] = useState("");
+
+  return (
+    <div className="flex justify-center gap-2">
+      <input
+        className="px-4 py-2 rounded text-black"
+        placeholder="GitHub username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button
+        onClick={() => onSearch(username)}
+        className="bg-blue-600 px-4 py-2 rounded"
+      >
+        Analyze
+      </button>
+    </div>
+  );
+}
